41957cdf14453e057b21dd8e53a61d6de3599536
cmroy4401 <cmroy4401@gmail.com>
2026-09-15 04:47:51 +0530

    Add files via upload

--- global_market.py
+++ global_market.py
@@ -6,8 +6,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 GLOBAL_CACHE = {
-    "SP500": {"symbol": "SP500", "ltp": 5850.00, "ch": 25.50, "chp": 0.44, "market_status": "RED"},
-    "NIKKEI": {"symbol": "NIKKEI", "ltp": 0.0, "ch": 0.0, "chp": 0.0, "market_status": "RED"},
     "DOW": {"symbol": "DOW", "ltp": 0.0, "ch": 0.0, "chp": 0.0, "market_status": "RED"},
     "DOW_FUT": {"symbol": "DOW_FUT", "ltp": 0.0, "market_status": "RED"}
 }
@@ -33,8 +31,7 @@ def fetch_tradingview_global(session, api_timeout, load_cache_func, save_cache_f
     payload = {
         "symbols": {
             "tickers": [
-                "TVC:DJI", "CBOT:YM1!", "TVC:IXIC", "TVC:SPX", "SP:SPX", "AMEX:SPY", "FOREXCOM:SPXUSD",
-                "TVC:NI225", "NYMEX:CL1!", "NYMEX:BZ1!", "TVC:GOLD"
+                "TVC:DJI", "CBOT:YM1!", "CBOT:YM", "DJ:DJI"
             ]
         },
         "columns": ["close", "change", "change_abs"]
@@ -65,20 +62,10 @@ def fetch_tradingview_global(session, api_timeout, load_cache_func, save_cache_f
                         if p <= 0: continue
                         item_data = {"ltp": round(p, 2), "ch": round(ch, 2), "chp": round(chp, 2)}
                         
-                        if "DJI" in s: GLOBAL_CACHE["DOW"] = {**item_data, "symbol": "DOW", "market_status": "RED"}
-                        elif "YM1!" in s: GLOBAL_CACHE["DOW_FUT"] = {"symbol": "DOW_FUT", "ltp": round(p, 2), "market_status": "RED"}
-                        elif "IXIC" in s: GLOBAL_CACHE["NASDAQ"] = {**item_data, "symbol": "NASDAQ", "market_status": "RED"}
-                        elif "SPX" in s or "SPXUSD" in s:
-                            if "SPY" in s and p < 1000:
-                                p *= 10; ch *= 10
-                                item_data = {"ltp": round(p, 2), "ch": round(ch, 2), "chp": chp}
-                            GLOBAL_CACHE["SP500"] = {**item_data, "symbol": "SP500", "market_status": "RED"}
-                        elif "NI225" in s: GLOBAL_CACHE["NIKKEI"] = {**item_data, "symbol": "NIKKEI", "market_status": "RED"}
-                        elif "CL1!" in s: GLOBAL_CACHE["WTI_CRUDE"] = {**item_data, "symbol": "WTI_CRUDE", "market_status": "RED"}
-                        elif "BZ1!" in s: GLOBAL_CACHE["BRENT"] = {**item_data, "symbol": "BRENT", "market_status": "RED"}
-                        elif "GOLD" in s:
-                            if p < 5000:
-                                GLOBAL_CACHE["XAUUSD"] = {**item_data, "symbol": "XAUUSD", "market_status": "RED"}
+                        if "DJI" in s: 
+                            GLOBAL_CACHE["DOW"] = {**item_data, "symbol": "DOW", "market_status": "RED"}
+                        elif "YM" in s: 
+                            GLOBAL_CACHE["DOW_FUT"] = {"symbol": "DOW_FUT", "ltp": round(p, 2), "market_status": "RED"}
                 
                 persisted = load_cache_func()
                 persisted["global"] = GLOBAL_CACHE
@@ -89,8 +76,6 @@ def fetch_tradingview_global(session, api_timeout, load_cache_func, save_cache_f
 @router.get("/api/global")
 def get_global(symbol: str):
     sym = symbol.upper()
-    if sym in ["SNP500", "SPX"]:
-        sym = "SP500"
     
     cached_global = load_cache_safely()
     for k, v in cached_global.items():
@@ -98,41 +83,11 @@ def get_global(symbol: str):
 
     if sym == "DOW":
         dow_data = dict(GLOBAL_CACHE.get("DOW", {"symbol": "DOW", "ltp": 0, "ch": 0, "chp": 0, "market_status": "RED"}))
-        fut_val = GLOBAL_CACHE.get("DOW_FUT", {}).get("ltp", 0)
+        fut_item = GLOBAL_CACHE.get("DOW_FUT", {})
+        fut_val = fut_item.get("ltp", 0) if isinstance(fut_item, dict) else fut_item
         if fut_val > 0:
             dow_data["fut"] = fut_val
         return dow_data
-
-    if sym in ["GOLD", "XAUUSD"]:
-        xau = GLOBAL_CACHE.get("XAUUSD", {"ltp": 0, "ch": 0, "chp": 0})
-        mcx = GLOBAL_CACHE.get("GOLD_MCX", {"ltp": 0, "ch": 0, "chp": 0})
-        return {
-            "symbol": "GOLD",
-            "ltp": xau.get("ltp", 0),
-            "ch": xau.get("ch", 0),
-            "chp": xau.get("chp", 0),
-            "inr": mcx.get("ltp", 0),
-            "inr_ch": mcx.get("ch", 0),
-            "inr_chp": mcx.get("chp", 0),
-            "market_status": "RED"
-        }
-
-    if sym in ["OIL", "CRUDE", "BRENT"]:
-        mcx_crude = GLOBAL_CACHE.get("CRUDE_MCX", {})
-        if not mcx_crude or mcx_crude.get("ltp", 0) == 0:
-            mcx_crude = GLOBAL_CACHE.get("WTI_CRUDE", {"ltp": 0, "ch": 0, "chp": 0})
-            
-        brent = GLOBAL_CACHE.get("BRENT", {"ltp": 0, "ch": 0, "chp": 0})
-        return {
-            "symbol": "OIL",
-            "ltp": mcx_crude.get("ltp", 0),
-            "ch": mcx_crude.get("ch", 0),
-            "chp": mcx_crude.get("chp", 0),
-            "brent_ltp": brent.get("ltp", 0),
-            "brent_ch": brent.get("ch", 0),
-            "brent_chp": brent.get("chp", 0),
-            "market_status": "RED"
-        }
 
     if sym in GLOBAL_CACHE:
         return GLOBAL_CACHE[sym]
